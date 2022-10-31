ecdd401e33f70ab3ad4e9c5198a9b8d576bc66e0
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2022-10-31 15:18:35 +0100

    Added firebase function

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -4,11 +4,10 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended", "google"],
   rules: {
+    // eslint-disable-next-line quote-props
     quotes: ["error", "double"],
+    "linebreak-style": 0,
   },
 };
